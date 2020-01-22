3b7f9773c0c5a146e488656ec6ad28fd74c399c4
Emanuel Catriquil <56092756+ecatriquil@users.noreply.github.com>
2020-01-22 09:30:21 -0300

    Crea archivos para la seccion basica del curso

---
+++ basicos/functions.py
@@ -0,0 +1,4 @@
+def suma_de_dos_numeros(x, y):
+    return x+y
+
+print(suma_de_dos_numeros(1, 2))
---
+++ basicos/turtle_square.py
@@ -0,0 +1,8 @@
+import turtle
+
+window = turtle.Screen()
+tortuga = turtle.Turtle()
+
+tortuga.forward(100)
+tortuga.right(90)
+window.mainloop()
